aced4c1977cfb6b469c6d5cb78b60870c8376e42
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-26 04:30:24 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,7 @@ def apply_clearance(cart)
   cart.each do |item, info|
     info.each do |category, value|
       if value == true && category == :clearance
-        result[item][:price] = ( cart[item][:price] - (cart[item][:price] * 0.2)).to_f
+        result[item][:price] = cart[item][:price] * 0.80 #( cart[item][:price] - (cart[item][:price] * 0.2)).to_f
       end
     end
   end
